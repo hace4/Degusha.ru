8ba06deb933552e116e4daef7522f35b2394fa60
hace4 <shmelkov03012006@gmail.com>
2023-07-03 20:58:00 +0300

    Db function

--- index.php
+++ index.php
@@ -1,16 +1,11 @@
 <?php
-
+use App\services\App;
 require_once __DIR__ . "/config/config.php";
-require_once __DIR__ . "/modules/database.php";
 //include composer autoload
 require_once __DIR__ . "/vendor/autoload.php";
-//connect to db
+//conect to db
 // include main file which taken alls views
 require_once __DIR__ . "/router/routes.php";
+App::connect();
 
-if ($_GET['q'] === 'sosi')
-{
-    $db = new database();
-    $db->set_users('2', 1, '1');
-    unset($db);
-}
+
---
+++ modules/user_interface.php
@@ -0,0 +1,4 @@
+<?php
+interface user_interface{
+
+}
